0487c0e1adc618d2a8875e28214fb4480b9bbebe
haopXpycary <haopxpycary@foxmail.com>
2018-03-11 01:04:27 +0800

    Update love.py

--- CodeForFree/love.py
+++ CodeForFree/love.py
@@ -1,3 +1,5 @@
-
+'''
+love.py -- 一行代码画爱心
+'''
 print('
 '.join([''.join([('+'[(x-y)%len('+')]if(x*0.05)**2*7+(-y*0.1)**2*5+(abs(x)*0.05)*(-y*0.1)*9<=5 else' ')for x in range(-30,30)])for y in range(15,-15,-1)]))
